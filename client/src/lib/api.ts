67c208448ca86352b107bf57dc88eb74c678493d
Nilanshu Garhewal <nilanshugarhewal1@gmail.com>
2026-02-11 10:30:57 +0530

    Framekit v1 Beta: Deleted unused images & fonts. Updated api.ts & post.ts and created a .env file for maintain the security of the api.

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,7 +1,12 @@
+// This file is use to fetch all the data from our backend api.
+// Do not alter or change the file until necessary.
+
+// Post is a type which is used by the function, so it means function is Post type.
+
 import type { Post } from "@/types/post";
 
 export async function getPosts(): Promise<Post[]> {
-  const res = await fetch("http://localhost:5000/api/posts", {
+  const res = await fetch(process.env.NEXT_PUBLIC_GET_POST_API_URL as string, {
     cache: "no-store",
   });
 
--- client/src/types/post.ts
+++ client/src/types/post.ts
@@ -1,3 +1,8 @@
+// This is the Post interface used as a Type to define any variable or value.
+// Means a variable is of Post type when used.
+
+// Do not alter or chnage the file until needed.
+
 export type Post = {
   id: string;
   imageUrl: string;
